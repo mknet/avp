5c8b32a40be4ffdb20b6128f0d171479b0efd418
Marcel Koch <kontak@marcelkoch.net>
2014-03-10 23:42:31 +0100

    Added flex slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,6 @@
-function Animal(id) {
+$(function() {
     'use strict';
-    this.id = id;
-}
 
-Animal.prototype.sound = function() {
-    'use strict';
-    var id = this.id;
-    console.log('id='+id);
-};
+    $('#slider').flexslider();
 
-var dog = new Animal(1);
-dog.sound();
+});
